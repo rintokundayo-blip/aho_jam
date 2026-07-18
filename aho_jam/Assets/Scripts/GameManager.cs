4108f7fa1390267b5e5d1f219edd12a9af4db9d5
ringo071955 <rintokundayo@gmail.com>
2026-07-18 16:43:27 +0900

    add

---
+++ aho_jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Texture2D handCursor;          // 手カーソル画像
+    public GameObject explanationPanel;   // 説明パネル
+
+    void Start()
+    {
+        // カーソルを手に変更
+        Cursor.SetCursor(handCursor, Vector2.zero, CursorMode.Auto);
+
+        // 説明パネルを表示
+        explanationPanel.SetActive(true);
+
+        // ゲーム停止
+        Time.timeScale = 0f;
+    }
+
+    public void StartGame()
+    {
+        // 説明パネルを非表示
+        explanationPanel.SetActive(false);
+
+        // ゲーム再開
+        Time.timeScale = 1f;
+    }
+}
